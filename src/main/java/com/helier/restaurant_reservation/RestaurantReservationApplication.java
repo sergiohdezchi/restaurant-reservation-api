f55749b6a673f2c58f53a550a52f0d2d9f8db6e2
sergio <sergiohdez.chi@gmail.com>
2025-04-14 15:45:15 -0600

    Create spring project

---
+++ src/main/java/com/helier/restaurant_reservation/RestaurantReservationApplication.java
@@ -0,0 +1,13 @@
+package com.helier.restaurant_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helier/restaurant_reservation/RestaurantReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helier.restaurant_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
